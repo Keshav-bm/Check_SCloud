c9c0484c2a37be7cb11177772b85bb877673b654
Keshav-bm <52231455+Keshav-bm@users.noreply.github.com>
2021-04-20 04:57:27 +0530

    Update Check_SCloudUITests.swift
    Test case class name modified

--- Check_SCloudUITests/Check_SCloudUITests.swift
+++ Check_SCloudUITests/Check_SCloudUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class Check_SCloudUITests: XCTestCase {
+class CheckSCloudTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
